edd0d34c3e8f65d164f86fea746eab7d49748476
areeyapholyiem <areeya.pho@kkumail.com>
2026-03-05 17:15:43 +0700

    feat addd login attemp log

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 def login(username, password):
-    """Basic login function"""
-    if username == "admin" and \
-       password == "secure_pw":
-        print("Login successful")
+print("Login attempt detected")
+if username == "admin":
+return "Login Success"
+return "Login Failed"
+print("Login attempt detected") 
