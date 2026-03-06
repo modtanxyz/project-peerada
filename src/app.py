02ad0ebc74b13918fb150b09dac12065a9dd9d97
areeyapholyiem <areeya.pho@kkumail.com>
2026-03-06 16:42:11 +0700

    feat: add login attempt log

--- src/app.py
+++ src/app.py
@@ -2,4 +2,10 @@ def login(username, password):
     print("Login attempt detected") # เพิ่ม
     if username == "admin":
         return "Login Success"
+<<<<<<< HEAD
     return "Login Failed"
+=======
+    return "Login Failed"
+
+print("Login attempt detected")
+>>>>>>> b7f83dd69a673bcd5dcd4ddf477ce6b84db90e19
